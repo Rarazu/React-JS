e12a94947298a72322c1960f18bdd15f1d6cce65
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2022-03-11 11:08:38 +0700

    Basic and Properties

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from "./components/Test"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h3 align="center">This is my custom components</h3>
+        {
+          /**
+           * nama element : h3
+           * attributes/properties: align
+           */
+        }
+
+        <Test name="Sunoo" city="Seoul" fontColor="magenta"
+        avatar="https://cdn.idntimes.com/content-images/community/2021/12/119864182-117068349984505-5038407251919109998-n-b619f653fb1c2b1965d5375ac1d27a66-9e898aec73d06ec3c8e400b6c8ba8fdc.jpg">
+          Sunoo adalah pacar saya
+          <button>Bismillah</button>
+        </Test>
+        <Test name="Beomgyu" city="Daegu" fontColor="gray"
+        avatar="https://i.pinimg.com/originals/67/73/49/677349b7f0497c1b7312f9e84b2c8ce4.jpg">
+          Beomgyu adalah suami saya  
+          <button>Aamiin</button>
+        </Test>
+        <Test name="Jay" city="Amerika" fontColor="navy"
+        avatar="https://assets.promediateknologi.com/crop/0x0:0x0/750x500/photo/hops/2021/10/919407DA-0E4F-421D-A6C9-D7D163CE76E1.jpeg">
+          Pengennya jadi adiknya Jay
+          <button>Imposibble</button>
+        </Test>
+      </div>
+
+      <div></div>
     </div>
   );
 }
---
+++ src/components/Test.js
@@ -0,0 +1,17 @@
+function Test(props) {
+    return(
+        <div style={ {color: props.fontColor, fontFamily:`Tahoma`} }>
+            <img src={props.avatar}
+            style={{width: `100px`, height: `100px`, borderRadius: '50%'}}>
+            </img>
+            <h4>Name : {props.name}</h4>
+            <h4>City : {props.city}</h4>
+            <p>
+                {props.children} 
+            </p>
+        </div>
+    )
+}
+// props.children = memanggil komponen properti anak
+
+export default Test; // agar bisa dipanggil file lain
